b3aae015694f7e0cedb928e8a2c8cd48093d45d8
roman.chehowski <r.chehowsky@gmail.com>
2017-12-04 11:05:07 +0300

    Rewritten in fluent functional style

--- dinner.py
+++ dinner.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python
 
 import random
+from functools import reduce
 
 
-def select(places):
-    max_probability = 0
-    for place in places:
-        max_probability += place[0]
-
-    num = random.uniform(0.0, max_probability)
-
-    accumulator = 0.0
-    for place in places:
-        current_chance = place[0]
-
-        if (num - accumulator) > current_chance:
-            accumulator += current_chance
-        else:
-            return f"{place[1]}, because {num}"
-
-    return "Unknown"
-
-all_places = [
-    (40, "Arbat"),
-    (40, "Myata"),
-    (15, "Lido"),
-    (10, "Comedia"),
-    (5, "Shaverma"),
-]
-
-print(select(all_places))
+def gen(places: dict):
+    accumulator = 0
+    for k, v in places.items():
+        accumulator += v
+        yield (k, (accumulator - v, accumulator))
 
 
+def select(places: dict):
+    random_num = int(random.uniform(0.0, reduce(lambda x, y: x + y, places.values())))
+
+    for i in gen(places):
+        place, rng = i[0], i[1]
+        if random_num in range(*rng):
+            return f'{place}, because {random_num}'
+
+    return 'I dunno, lol'
+
+
+def main():
+    all_places = {
+        'Litwiny': 40,
+        'Terra pizza': 40,
+        'Arena city': 20
+    }
+
+    print(select(all_places))
+
+
+if __name__ == '__main__':
+    main()
